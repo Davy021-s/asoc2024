8cd2fe8ed56c406aa8e61e30ed0334f00f4125c1
vinc.pii <vinc.pii@gmail.com>
2024-08-15 07:40:01 +0000

    Split out json function from previous blob.

--- static/bookings.js
+++ static/bookings.js
@@ -1,30 +1,32 @@
 fetch('/api/bookings')
   .then(response => response.json())
   .then(bookings => {
-    const table = document.createElement('table');
-    table.innerHTML = `
-      <thead>
-        <tr>
-          <th>Booking ID</th>
-          <th>Court ID</th>
-          <th>User Name</th>
-          <th>Date</th>
-          <th>Start Hour</th>
-          <th>End Hour</th>
-        </tr>
-      </thead>
-      <tbody>
-        ${bookings.map(booking => `
-          <tr>
-            <td>${booking.booking_id}</td>
-            <td>${booking.court_id}</td>
-            <td>${booking.user_name}</td>
-            <td>${booking.date}</td>
-            <td>${booking.start_hour}</td>
-            <td>${booking.end_hour}</td>
-          </tr>
-        `).join('')}
-      </tbody>
-    `;
-    document.getElementById('bookings-table').appendChild(table);
+    showBookingsTable(bookings);
   });
+
+function showBookingsTable(bookings) {
+  const table = document.createElement('table');
+  table.innerHTML = `
+    <thead>
+      <tr>
+        <th>Court ID</th>
+        <th>User Name</th>
+        <th>Date</th>
+        <th>Start Hour</th>
+        <th>End Hour</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${bookings.map(booking => `
+        <tr>
+          <td>${booking.court_id}</td>
+          <td>${booking.user_name}</td>
+          <td>${booking.date}</td>
+          <td>${booking.start_hour}</td>
+          <td>${booking.end_hour}</td>
+        </tr>
+      `).join('')}
+    </tbody>
+  `;
+  document.getElementById('bookings-table').appendChild(table);
+}
